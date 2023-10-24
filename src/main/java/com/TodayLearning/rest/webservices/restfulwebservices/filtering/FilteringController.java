f18039c02f943cfa127f28e917650f0714d67cc4
Yogendra <todaylearning@gmail.com>
2023-10-24 11:38:02 +0530

    Implementation of DYNAMIC FILTERING FOER THE SOMEBEAN AND SOMEBEAN LIST

--- src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,26 +1,47 @@
 package com.TodayLearning.rest.webservices.restfulwebservices.filtering;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController 
 {
 	@GetMapping("/filtering")
-     public SomeBean filtering()
+     public MappingJacksonValue filtering()   // want to return field1 and field3 
      {
-    	 return new SomeBean("Value1","Value2","Value3");
+		SomeBean someBean = new SomeBean("Value1","Value2","Value3");
+		
+		//MappingJacksonValue 
+		MappingJacksonValue mappingJacksonValue=new MappingJacksonValue(someBean);
+    	 
+		SimpleBeanPropertyFilter filter=SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+		FilterProvider filters= new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+		mappingJacksonValue.setFilters(filters);
+		return mappingJacksonValue;
      }
 	
 	//field2 as we make the json ignore so its not even taken in the list 
 	@GetMapping("/filtering-list")
-    public  List<SomeBean> filteringList()
+    public  MappingJacksonValue filteringList()  // want to return field2 and field3 
     {
-   	 return Arrays.asList(new SomeBean("Value1", "Value2", "Value3"),
-   			                new SomeBean("Value4", "Value5", "Value6"));
+   	 List<SomeBean> asList = Arrays.asList(new SomeBean("Value1", "Value2", "Value3"),
+				                new SomeBean("Value4", "Value5", "Value6"));
+   	 
+   	 MappingJacksonValue mappingJacksonValue=new MappingJacksonValue(asList);
+   	 
+   	 
+   	SimpleBeanPropertyFilter filter=SimpleBeanPropertyFilter.filterOutAllExcept("field2","field3");
+	FilterProvider filters=new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+	mappingJacksonValue.setFilters(filters);
+   	 
+   	 return mappingJacksonValue;
     }
 }
--- src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,20 +1,25 @@
 package com.TodayLearning.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 //this is another annotation in optioal to @JsonIgnore , you can specify on class 
 // @JsonIgnoreProperties("field1")                //you can define it at the class level , you can directly able to add the value here
 // so you can either use @JsonIgnore OR use @JsonIgnoreProperties so achieve the "STATIC FILTERING...."
 // if you want to specify multiple values in @JsonIgnoreProperties use {"field1","field2"}
-// best option -> @JsonIgnore , as in future if the field name changes then also we doesn't need to worry there 
+// best option -> @JsonIgnore , as in future if the field name changes then also we doesn't need to worry there
+
+@JsonFilter("SomeBeanFilter")
+//@JsonFilter("SomeBeanFilterList") : You cannot do so , keep the filters names as same 
 public class SomeBean 
 {
      private String field1;
      
      //example of static filtering - suppose field2 is password 
      // NOTE: this "field2" not even taken in the list if we tried to add it in the list 
-     @JsonIgnore
+     //@JsonIgnore
      private String field2;
+     
+    // @JsonIgnore
      private String field3;
      
 	public SomeBean(String field1, String field2, String field3) {
