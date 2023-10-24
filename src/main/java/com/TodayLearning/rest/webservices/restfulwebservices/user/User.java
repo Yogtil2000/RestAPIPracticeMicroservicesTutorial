be646b30963f62c1ed068703d309184b5a7a832f
Yogendra <todaylearning@gmail.com>
2023-10-24 10:21:03 +0530

    customizing the rest api response representations

--- src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/TodayLearning/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.TodayLearning.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,7 @@ public class User
 {
        private int id;
        @Size(min=2,message = "Name should have atleast 2 characters")
+       @JsonProperty("user_name")
        private String name;
        
        @Past(message = "Birthdate should be in past")
